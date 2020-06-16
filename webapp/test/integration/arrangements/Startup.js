8245e706345f6e2ca0daecf1300cf00bd31cd451
Matthias Osswald <mat.osswald@sap.com>
2020-06-16 12:56:22 +0200

    Demo: iframe coverage with karma-ui5

--- webapp/test/integration/arrangements/Startup.js
+++ webapp/test/integration/arrangements/Startup.js
@@ -6,13 +6,7 @@ sap.ui.define([
 	return Opa5.extend("sap.ui.demo.todo.test.integration.arrangements.Startup", {
 
 		iStartMyApp: function () {
-			this.iStartMyUIComponent({
-				componentConfig: {
-					name: "sap.ui.demo.todo",
-					async: true,
-					manifest: true
-				}
-			});
+			this.iStartMyAppInAFrame(sap.ui.require.toUrl("sap/ui/demo/todo/index") + ".html");
 		}
 
 	});
